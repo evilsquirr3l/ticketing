19c477391e8f586ec2a1c794f7d91da33aaae4e8
Serhii Denshchyk <serhii.denshchyk@gmail.com>
2023-11-26 21:28:36 +0100

    Add GET /events/{event_id}/sections/{section_id}/seats endpoint

---
+++ Ticketing/Features/Seats/GetSeatsInSection.cs
@@ -0,0 +1,58 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ticketing.Data;
+using Ticketing.Models;
+
+namespace Ticketing.Features.Seats;
+
+[ApiController]
+[ApiExplorerSettings(GroupName = "Seats")]
+public class GetSeatsInSection : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public GetSeatsInSection(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpGet]
+    [Route("/events/{eventId:guid}/sections/{sectionId:guid}/seats")]
+    public async Task<IResult> GetAllSeats(Guid eventId, Guid sectionId)
+    {
+        var result = await _mediator.Send(new GetAllSeatsQuery(eventId, sectionId));
+
+        return Results.Ok(result);
+    }
+    
+    public record GetAllSeatsQuery(Guid EventId, Guid SectionId) : IRequest<IEnumerable<SeatViewModel>>;
+    
+    public class GetAllSeatsQueryHandler : IRequestHandler<GetAllSeatsQuery, IEnumerable<SeatViewModel>>
+    {
+        private readonly TicketingDbContext _dbContext;
+
+        public GetAllSeatsQueryHandler(TicketingDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    
+        public async Task<IEnumerable<SeatViewModel>> Handle(GetAllSeatsQuery request, CancellationToken cancellationToken)
+        {
+            var seats = await _dbContext.Manifests
+                .Include(x => x.Venue)
+                .Where(x => x.Venue.EventId == request.EventId)
+                .SelectMany(x => x.Sections)
+                .Include(x => x.Rows)
+                .ThenInclude(x => x.Seats)
+                .SelectMany(x => x.Rows)
+                .SelectMany(x => x.Seats)
+                .ToListAsync(cancellationToken: cancellationToken);
+
+            var seatsViewModel = seats.Select(seat =>
+                new SeatViewModel(seat.Id, seat.SeatNumber));
+
+            return seatsViewModel;
+        }
+    }
+}
