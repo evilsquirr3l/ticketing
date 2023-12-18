ca511c02f439db6ca394398c39157ff779db7ce3
Serhii Denshchyk <serhii.denshchyk@gmail.com>
2023-12-18 18:01:42 +0100

    Remove useless usings

--- NotificationHandler/Program.cs
+++ NotificationHandler/Program.cs
@@ -1,8 +1,3 @@
-using Azure.Identity;
-using Azure.Messaging.ServiceBus;
-using Microsoft.Extensions.Azure;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
